72cc62fcf8ee80eb21299e85d461e6f817e47297
Wassim <wasshimu.dev@gmail.com>
2024-11-30 14:24:35 +0100

    Ajout ecouteur evenement on scroll

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('connecté !')
+const modal = document.querySelector('.modal-container');
+const btn = document.querySelector('.btn-success');
+const code = document.querySelector('.code');
+const icone = document.querySelector('.fa-times');
+
+window.addEventListener('scroll', function(){
+    console.log('defilement détécté');
+    let hauteur = this.document.documentElement.scrollTop;
+    console.log(hauteur, 'nombre de pixels depuis le haut de la page');
+});
