a943cd5e73d7110e264c688a1262e8d6c5b81a9b
Wassim <wasshimu.dev@gmail.com>
2024-12-02 15:44:59 +0100

    Finalisation, ajout fonction + remove popup

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ const btn = document.querySelector('.btn-success');
 const code = document.querySelector('.code');
 const icone = document.querySelector('.fa-times');
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', displayModal);
+
+function displayModal(){
     console.log('defilement détécté');
-    let hauteur = this.document.documentElement.scrollTop;
+    let hauteur = document.documentElement.scrollTop;
     console.log(hauteur, 'nombre de pixels depuis le haut de la page');
     if(hauteur > 400){
-        modal.style.display = ('flex')
-    }
-});
+        modal.style.display = "flex"
+    };
+};
 
 btn.addEventListener('click', function(){
-    code.style.display = ('block')
-    btn.style.display = ('none')
-})
+    code.style.display = 'block';
+    btn.style.display = 'none';
+});
+
+icone.addEventListener('click', function(){
+    modal.style.display = ('none')
+    window.removeEventListener('scroll', displayModal)
+});
