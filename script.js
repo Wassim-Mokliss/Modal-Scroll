5f626af75c806cc14743aef9d6c5010c55eefb87
Wassim <wasshimu.dev@gmail.com>
2024-12-02 14:51:59 +0100

    Ajout ecouteurs evenement on scroll et click

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ window.addEventListener('scroll', function(){
     console.log('defilement détécté');
     let hauteur = this.document.documentElement.scrollTop;
     console.log(hauteur, 'nombre de pixels depuis le haut de la page');
+    if(hauteur > 400){
+        modal.style.display = ('flex')
+    }
 });
+
+btn.addEventListener('click', function(){
+    code.style.display = ('block')
+    btn.style.display = ('none')
+})
